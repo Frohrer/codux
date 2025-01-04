5ee88e50b301d12a1e58c444f49f3f94c163a5f0
Frederic Rohrer <github@frohrer.com>
2025-01-03 21:19:02 -0500

    ignore home dir

--- piston/repo/packages/node/20.11.1/build.sh
+++ piston/repo/packages/node/20.11.1/build.sh
@@ -9,10 +9,11 @@ tar xf node.tar.xz --strip-components=1
 # Clean up the archive
 rm node.tar.xz
 
-# Create npm wrapper that bypasses user config checks
+# Create npm wrapper that forces a system root
 cat > bin/npm << 'EOF'
 #!/bin/bash
-node "$PWD/lib/node_modules/npm/bin/npm-cli.js" --no-user --prefix "$PWD" "$@"
+export SYSTEMROOT=/box/submission
+node "$PWD/lib/node_modules/npm/bin/npm-cli.js" "$@"
 EOF
 
 chmod +x bin/npm
