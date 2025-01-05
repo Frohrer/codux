71f12f6b530cbd58f9cc6d5a9213342a1ac01a59
Frederic Rohrer <github@frohrer.com>
2025-01-04 20:34:38 -0500

    make public proxy url configurable

--- piston/api/src/proxy-handler.js
+++ piston/api/src/proxy-handler.js
@@ -1,97 +1,96 @@
-const express = require('express');
-const cors = require('cors');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const { v4: uuidv4 } = require('uuid');
-const logger = require('logplease').create('proxy-handler');
+const express = require("express");
+const cors = require("cors");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const { v4: uuidv4 } = require("uuid");
+const logger = require("logplease").create("proxy-handler");
+const config = require("./config");
 
 class ProxyManager {
-    constructor() {
-        if (ProxyManager.instance) {
-            return ProxyManager.instance;
-        }
-        ProxyManager.instance = this;
+	constructor() {
+		if (ProxyManager.instance) {
+			return ProxyManager.instance;
+		}
+		ProxyManager.instance = this;
 
-        this.proxies = new Map();
-        this.nextPort = 10001;
+		this.proxies = new Map();
+		this.nextPort = 10001;
 
-        this.app = express();
-        this.app.use(cors());
-        this.server = require('http').createServer(this.app);
+		this.app = express();
+		this.app.use(cors());
+		this.server = require("http").createServer(this.app);
 
-        this.setupLogging();
-        
-        this.server.listen(2020, () => {
-            logger.info('Proxy server running on port 2020');
-        });
+		this.setupLogging();
 
-        return this;
-    }
+		this.server.listen(2020, () => {
+			logger.info("Proxy server running on port 2020");
+		});
 
-    setupLogging() {
-        setInterval(() => {
-            const proxies = Array.from(this.proxies.entries())
-                .map(([id, info]) => `${id}: ${info.path} -> ${info.port}`)
-                .join('\n');
-            logger.debug(`Current active proxies:\n${proxies}`);
-        }, 10000);
-    }
+		return this;
+	}
 
-    getNextAvailablePort() {
-        return this.nextPort++;
-    }
+	setupLogging() {
+		setInterval(() => {
+			const proxies = Array.from(this.proxies.entries())
+				.map(([id, info]) => `${id}: ${info.path} -> ${info.port}`)
+				.join("\n");
+			logger.debug(`Current active proxies:\n${proxies}`);
+		}, 10000);
+	}
 
-    createProxy(jobId) {
-        const port = this.getNextAvailablePort();
-        const proxyId = uuidv4();
-        const proxyPath = `/proxy/${proxyId}`;
-    
-        const proxyInfo = {
-            port,
-            path: proxyPath,
-            active: true,
-            id: proxyId
-        };
-    
-        const proxy = createProxyMiddleware({
-            target: `http://localhost:${port}`,
-            changeOrigin: true,
-            ws: true,
-            pathFilter: proxyPath,
-            logger: logger,
-            onProxyReq: (proxyReq, req, res) => {
-            
-                logger.debug('Proxying request:', {
-                    from: req.originalUrl,
-                    to: `http://localhost:${port}${req.originalUrl}`,
-                    headers: proxyReq.getHeaders(),
-                });
-            }
-        });
-    
-        this.app.use(proxy);
-        this.server.on('upgrade', proxy.upgrade);
-        this.proxies.set(jobId, proxyInfo);
-        logger.debug(`Created proxy: ${jobId} -> ${JSON.stringify(proxyInfo)}`);
-    
-        return proxyInfo;
-    }
-    
+	getNextAvailablePort() {
+		return this.nextPort++;
+	}
 
-    removeProxy(jobId) {
-        const proxyInfo = this.proxies.get(jobId);
-        if (proxyInfo) {
-            logger.debug(`Removing proxy: ${jobId} -> ${JSON.stringify(proxyInfo)}`);
-            this.proxies.delete(jobId);
-        }
-    }
+	createProxy(jobId) {
+		const port = this.getNextAvailablePort();
+		const proxyId = uuidv4();
+		const proxyPath = `/proxy/${proxyId}`;
 
-    getProxyInfo(jobId) {
-        return this.proxies.get(jobId);
-    }
+		const proxyInfo = {
+			port,
+			path: proxyPath,
+			active: true,
+			id: proxyId,
+		};
 
-    getBaseUrl() {
-        return 'http://localhost:2020';
-    }
+		const proxy = createProxyMiddleware({
+			target: `http://localhost:${port}`,
+			changeOrigin: true,
+			ws: true,
+			pathFilter: proxyPath,
+			logger: logger,
+			onProxyReq: (proxyReq, req, res) => {
+				logger.debug("Proxying request:", {
+					from: req.originalUrl,
+					to: `http://localhost:${port}${req.originalUrl}`,
+					headers: proxyReq.getHeaders(),
+				});
+			},
+		});
+
+		this.app.use(proxy);
+		this.server.on("upgrade", proxy.upgrade);
+		this.proxies.set(jobId, proxyInfo);
+		logger.debug(`Created proxy: ${jobId} -> ${JSON.stringify(proxyInfo)}`);
+
+		return proxyInfo;
+	}
+
+	removeProxy(jobId) {
+		const proxyInfo = this.proxies.get(jobId);
+		if (proxyInfo) {
+			logger.debug(`Removing proxy: ${jobId} -> ${JSON.stringify(proxyInfo)}`);
+			this.proxies.delete(jobId);
+		}
+	}
+
+	getProxyInfo(jobId) {
+		return this.proxies.get(jobId);
+	}
+
+	getBaseUrl() {
+		return config.proxy_domain;
+	}
 }
 
 module.exports = ProxyManager;
