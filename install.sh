0e5ac2bbb8f71cc0cb348461a16934d3c3a5f675
Frederic Rohrer <github@frohrer.com>
2025-01-09 12:59:37 -0500

    only install streamlit

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -e
 chmod +x ./piston/repo/piston
 # Build packages
-./piston/repo/piston build-pkg node 20.11.1
+# ./piston/repo/piston build-pkg node 20.11.1
 # ./piston/repo/piston build-pkg node 15.10.0
 # ./piston/repo/piston build-pkg node 16.3.0
 # ./piston/repo/piston build-pkg node 18.15.0
@@ -12,7 +12,7 @@ chmod +x ./piston/repo/piston
 # ./piston/repo/piston build-pkg python 3.9.4
 # ./piston/repo/piston build-pkg python 3.10.0
 # ./piston/repo/piston build-pkg python 3.11.11
-# ./piston/repo/piston build-pkg streamlit 3.11.0
+ ./piston/repo/piston build-pkg streamlit 3.11.0
 # ./piston/repo/piston build-pkg python 3.12.8
 # ./piston/repo/piston build-pkg python 3.13.1
 # ./piston/repo/piston build-pkg bash 5.2.0
